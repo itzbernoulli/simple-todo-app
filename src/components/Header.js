8156250f7b944df150a478ececd98959131f23a6
Ayodeji <oyinloyeayodeji@gmail.com>
2020-08-10 05:28:14 +0100

    first chapter complete

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Simple Todo App</h1>
+            <p>Please add to_dos item(s) through the input field</p>
+        </header>
+    )
+}
+export default Header
