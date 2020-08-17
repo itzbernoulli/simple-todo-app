e7ede1350f374cae0bc6d30a68d1ad1165db2ded
Ayodeji <oyinloyeayodeji@gmail.com>
2020-08-17 05:26:57 +0100

    Updates

---
+++ precache-manifest.eac5561e8705b36c12e3bec1858e2bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a15e81a38006e8efd633c087f031b76",
+    "url": "/itzbernoulli/simple-todo-app/index.html"
+  },
+  {
+    "revision": "2147e02605e242700880",
+    "url": "/itzbernoulli/simple-todo-app/static/css/main.8694691b.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/itzbernoulli/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/itzbernoulli/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2147e02605e242700880",
+    "url": "/itzbernoulli/simple-todo-app/static/js/main.a2f81c11.chunk.js"
+  },
+  {
+    "revision": "ab83386326db3084376f",
+    "url": "/itzbernoulli/simple-todo-app/static/js/runtime-main.2fb711fc.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/itzbernoulli/simple-todo-app/precache-manifest.eac5561e8705b36c12e3bec1858e2bbb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/itzbernoulli/simple-todo-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
